183d9b190d465b0baed23e887761514fd43ddac2
bryanwaine <nwanne.ezeaka@gmail.com>
2025-04-19 02:09:08 +0100

    Update textarea  style

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import Button from "./Button";
 import ArrowUpwardOutlinedIcon from "@mui/icons-material/ArrowUpwardOutlined";
-import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
 const TextArea = ({ value, onChange, onSubmit, loading }) => {
   const textareaRef = useRef(null);
 
@@ -13,9 +13,8 @@ const TextArea = ({ value, onChange, onSubmit, loading }) => {
     }
   }, [value]);
 
-
   const handleKeyDown = (event) => {
-    if(loading) return
+    if (loading) return;
     if (event.key === "Enter" && !event.shiftKey) {
       event.preventDefault();
       onSubmit();
@@ -23,28 +22,38 @@ const TextArea = ({ value, onChange, onSubmit, loading }) => {
   };
 
   return (
-    <div className="textarea-container">
-      <textarea
-        ref={textareaRef}
-        rows={2}
-        className="textarea-input"
-        placeholder="Ask anything..."
-        id="question"
-        name="question"
-        value={value}
-        onChange={onChange}
-        onKeyDown={handleKeyDown}
-        required
-      />
-      <div className="textarea-buttons">
-        <Button variant="ghost--orange" onClick={onSubmit} ariaLabel="submit" className="submit-btn">
-          <AddOutlinedIcon fontSize="small" />
-        </Button>
-        <Button variant="orange" onClick={onSubmit} ariaLabel="submit"
-          disabled={!value || loading}
-        >
-          <ArrowUpwardOutlinedIcon fontSize="small" />
-        </Button>
+    <div className="textarea-wrapper">
+      <div className="textarea-container">
+        <textarea
+          ref={textareaRef}
+          rows={2}
+          className="textarea-input"
+          placeholder="Ask anything..."
+          id="question"
+          name="question"
+          value={value}
+          onChange={onChange}
+          onKeyDown={handleKeyDown}
+          required
+        />
+        <div className="textarea-buttons">
+          <Button
+            variant="ghost--orange"
+            onClick={onSubmit}
+            ariaLabel="submit"
+            className="submit-btn"
+          >
+            <AddOutlinedIcon fontSize="small" />
+          </Button>
+          <Button
+            variant="orange"
+            onClick={onSubmit}
+            ariaLabel="submit"
+            disabled={!value || loading}
+          >
+            <ArrowUpwardOutlinedIcon fontSize="small" />
+          </Button>
+        </div>
       </div>
     </div>
   );
