c32b1078dd152df035ee494b6717deab7f076b00
bryanwaine <nwanne.ezeaka@gmail.com>
2025-05-08 10:16:14 +0100

    Add media queries to layout, home and dashboard styles

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -11,14 +11,14 @@ const Sidebar = ({ sidebarOpen, handleLogout, userName, email }) => {
         <ul className="sidebar-list">
           <h3>ACCOUNT</h3>
           <li>
-            <div>
+            <div className="sidebar-list-item">
               <PersonOutlineIcon className="icon" />
               <span>Name</span>
             </div>
             <p>{userName}</p>
           </li>
           <li>
-            <div>
+            <div className="sidebar-list-item">
               <MailOutlineIcon className="icon" />
               <span>Email</span>
             </div>
@@ -28,14 +28,14 @@ const Sidebar = ({ sidebarOpen, handleLogout, userName, email }) => {
         <ul className="sidebar-list">
           <h3>APP</h3>
           <li>
-            <div>
+            <div className="sidebar-list-item">
               <DarkModeOutlinedIcon className="icon" />
               <span> Color Scheme</span>
             </div>
             <p>Light</p>
           </li>
           <li>
-            <div>
+            <div className="sidebar-list-item">
               <LogoutIcon className="icon" />
               <Link onClick={handleLogout} className="link">
                 Logout
--- src/utils/greetingHandler.js
+++ src/utils/greetingHandler.js
@@ -1,11 +1,11 @@
 const handleGreeting = (name) => {
   const hour = new Date().getHours();
   if (hour < 12) {
-    return `Good Morning, ${name}.`;
+    return `Good Morning, ${name}`;
   } else if (hour < 18) {
-    return `Good Afternoon, ${name}.`;
+    return `Good Afternoon, ${name}`;
   } else {
-    return `Good Evening, ${name}.`;
+    return `Good Evening, ${name}`;
   }
 };
 
