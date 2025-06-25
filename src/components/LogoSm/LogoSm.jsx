3080fdbf1e2b8d69f7d914e014525ee0050ba0db
bryanwaine <nwanne.ezeaka@gmail.com>
2025-06-25 10:42:48 +0100

    Add tailwind classes for sidebar

--- src/components/LogoSm/LogoSm.jsx
+++ src/components/LogoSm/LogoSm.jsx
@@ -5,7 +5,7 @@ import "./LogoSm.css";
 const LogoSm = ({variant}) => {
   return (
     <Link to="/" className="bg-[#001826] flex justify-center items-center !px-2 !py-1 border-none">
-      <p className={`logo-${variant} !text-white text-bold text-2xl`}>
+      <p className={`logo logo-${variant} !text-white text-bold text-2xl`}>
       Auxili<span className="text-[#FF7B00]">ai</span>re
       </p>
     </Link>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -8,38 +8,38 @@ import "./Sidebar.css";
 
 const Sidebar = ({ sidebarOpen, handleLogout, userName, email }) => {
   return (
-    <div className="sidebar" data-sidebar-open={sidebarOpen}>
-      <h3 className="sidebar__list-title">ACCOUNT</h3>
+    <div className="sidebar bg-gray-100/90 dark:bg-neutral-900/90  backdrop-blur-md" data-sidebar-open={sidebarOpen}>
+      <h3 className="text-md align-left self-start !ml-4 text-sky-900 dark:text-sky-100 font-light">ACCOUNT</h3>
       <ul className="sidebar__list">
-        <li>
-          <div className="sidebar__item">
+        <li className="bg-sky-100 dark:bg-black">
+           <div className="w-full flex-1 flex items-center gap-2 text-sky-900 dark:text-sky-100">
             <PersonOutlineIcon fontSize="small" className="sidebar__icon" />
-            <span className="sidebar__item-name">Name</span>
+            <span className="font-md text-sm"> Name</span>
           </div>
-          <p className="sidebar__item-value">{userName}</p>
+          <p className="sidebar__item-value w-full flex-3 flex justify-end items-center gap-2 text-xs font-light text-neutral-900 dark:text-neutral-100">{userName}</p>
         </li>
-        <li>
-          <div className="sidebar__item">
+       <li className="bg-sky-100 dark:bg-black">
+           <div className="w-full flex-1 flex items-center gap-2 text-sky-900 dark:text-sky-100">
             <MailOutlineIcon fontSize="small" className="sidebar__icon" />
-            <span className="sidebar__item-name">Email</span>
+            <span className="font-md text-sm"> Email</span>
           </div>
-          <p className="sidebar__item-value">{email}</p>
+          <p className="sidebar__item-value w-full flex-3 flex justify-end items-center gap-2 text-xs font-light text-neutral-900 dark:text-neutral-100 ">{email}</p>
         </li>
       </ul>
-      <h3 className="sidebar__list-title">APP</h3>
+      <h3 className="text-md align-left self-start !ml-4 text-sky-900 dark:text-sky-100 font-light">APP</h3>
       <ul className="sidebar__list">
-        <li>
-          <div className="sidebar__item">
-            <DarkModeOutlinedIcon fontSize="small" className="sidebar__icon" />
-            <span className="sidebar__item-name"> Color Scheme</span>
+        <li className="bg-sky-100 dark:bg-black">
+          <div className="w-full flex items-center gap-2 text-sky-900 dark:text-sky-100">
+            <DarkModeOutlinedIcon fontSize="small" className="text-md" />
+            <span className="font-md text-sm"> Color Scheme</span>
           </div>
-          <p className="sidebar__item-value">Light</p>
+          <p className="sidebar__item-value w-full flex justify-end items-center gap-2 text-xs font-light text-neutral-900 dark:text-neutral-100 truncate overflow-hidden whitespace-nowrap">Light</p>
         </li>
-        <li>
-          <div className="sidebar__item">
-            <LogoutIcon fontSize="small" className="sidebar__icon" />
-            <Link onClick={handleLogout} className="link">
-              <span className="sidebar__item-name">Logout</span>
+         <li className="bg-sky-100 dark:bg-black">
+          <div className="w-full flex items-center gap-2 text-sky-900 dark:text-sky-100">
+            <LogoutIcon fontSize="small" className="text-md" />
+            <Link onClick={handleLogout} className="">
+              <span className="font-md text-sm">Logout</span>
             </Link>
           </div>
         </li>
