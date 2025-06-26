6b6247c3938367e77ed997d737691fccd4eccb1d
bryanwaine <nwanne.ezeaka@gmail.com>
2025-06-26 17:08:38 +0100

    Update home cta message

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -101,7 +101,7 @@ const Home = () => {
                 </p>
           <Link to="/signup" className="animate w-full md:w-1/5 !mb-20">
             <Button variant="orange" className="!mt-4 !mb-20">
-              Start Studying
+              Start Studying Now
             </Button>
           </Link>
         </section>
