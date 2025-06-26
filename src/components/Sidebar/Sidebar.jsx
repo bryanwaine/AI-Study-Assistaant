d70648bd6321ba603856902715ec490211e0dc8b
bryanwaine <nwanne.ezeaka@gmail.com>
2025-06-26 19:29:28 +0100

    Update color scheme in side bar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -8,34 +8,51 @@ import "./Sidebar.css";
 
 const Sidebar = ({ sidebarOpen, handleLogout, userName, email }) => {
   return (
-    <div className="sidebar bg-gray-100/90 dark:bg-neutral-900/90  backdrop-blur-md" data-sidebar-open={sidebarOpen}>
-      <h3 className="text-md align-left self-start !ml-4 text-sky-900 dark:text-sky-400 font-light">ACCOUNT</h3>
+    <div
+      className="sidebar bg-gray-100/90 dark:bg-neutral-900/90  backdrop-blur-md"
+      data-sidebar-open={sidebarOpen}
+    >
+      <h3 className="text-md align-left self-start !ml-4 text-sky-900 dark:text-sky-400 font-light">
+        ACCOUNT
+      </h3>
       <ul className="sidebar__list">
         <li className="bg-sky-100 dark:bg-black">
-           <div className="w-full flex-1 flex items-center gap-2 text-sky-900 dark:text-sky-400">
+          <div className="w-full flex-1 flex items-center gap-2 text-sky-900 dark:text-sky-400">
             <PersonOutlineIcon fontSize="small" className="sidebar__icon" />
             <span className="font-md text-sm"> Name</span>
           </div>
-          <p className="sidebar__item-value w-full flex-3 flex justify-end items-center gap-2 text-xs font-light text-neutral-900 dark:text-neutral-100">{userName}</p>
+          <p className="sidebar__item-value w-full flex-3 flex justify-end items-center gap-2 text-xs font-light text-neutral-900 dark:text-neutral-100">
+            {userName}
+          </p>
         </li>
-       <li className="bg-sky-100 dark:bg-black">
-           <div className="w-full flex-1 flex items-center gap-2 text-sky-900 dark:text-sky-400">
+        <li className="bg-sky-100 dark:bg-black">
+          <div className="w-full flex-1 flex items-center gap-2 text-sky-900 dark:text-sky-400">
             <MailOutlineIcon fontSize="small" className="sidebar__icon" />
             <span className="font-md text-sm"> Email</span>
           </div>
-          <p className="sidebar__item-value w-full flex-3 flex justify-end items-center gap-2 text-xs font-light text-neutral-900 dark:text-neutral-100 ">{email}</p>
+          <p className="sidebar__item-value w-full flex-3 flex justify-end items-center gap-2 text-xs font-light text-neutral-900 dark:text-neutral-100 ">
+            {email}
+          </p>
         </li>
       </ul>
-      <h3 className="text-md align-left self-start !ml-4 text-sky-900 dark:text-sky-400 font-light">APP</h3>
+      <h3 className="text-md align-left self-start !ml-4 text-sky-900 dark:text-sky-400 font-light">
+        APP
+      </h3>
       <ul className="sidebar__list">
         <li className="bg-sky-100 dark:bg-black">
           <div className="w-full flex items-center gap-2 text-sky-900 dark:text-sky-400">
             <DarkModeOutlinedIcon fontSize="small" className="text-md" />
             <span className="font-md text-sm"> Color Scheme</span>
           </div>
-          <p className="sidebar__item-value w-full flex justify-end items-center gap-2 text-xs font-light text-neutral-900 dark:text-neutral-100 truncate overflow-hidden whitespace-nowrap">System</p>
+          <div className="sidebar__item-value w-full flex justify-end items-center gap-2 text-xs font-light text-neutral-900 dark:text-neutral-100 ">
+            <select name="color-scheme" id="color-scheme">
+              <option value="system">System</option>
+              <option value="light">Light</option>
+              <option value="dark">Dark</option>
+            </select>
+          </div>
         </li>
-         <li className="bg-sky-100 dark:bg-black">
+        <li className="bg-sky-100 dark:bg-black">
           <div className="w-full flex items-center gap-2 text-sky-900 dark:text-sky-400">
             <LogoutIcon fontSize="small" className="text-md" />
             <Link onClick={handleLogout} className="">
