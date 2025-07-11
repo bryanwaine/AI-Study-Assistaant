d38f92572a908ac80f308e5bbbe0e4ebd3b12791
bryanwaine <nwanne.ezeaka@gmail.com>
2025-07-11 11:03:13 +0100

    Update dashboard overview skeleton

--- src/components/Skeleton/DashboardOverviewSkeleton.jsx
+++ src/components/Skeleton/DashboardOverviewSkeleton.jsx
@@ -3,19 +3,19 @@ import './Skeleton.css'
 const DashboardOverviewSkeleton = () => {
   return (
     <div className="dashboard-card__overview">
-      <div className="skeleton__card dashboard-card__item rounded-xl bg-neutral-100 dark:bg-gray-900/10 backdrop-blur shadow-md border border-neutral-200 dark:border-none">
+      <div className="skeleton__card dashboard-card__item rounded-xl bg-neutral-100 dark:bg-gray-100/10 backdrop-blur shadow-md border border-neutral-200 dark:border-none">
         <span className="skeleton skeleton__card-label dashboard-card__item-label" />
         <span className="skeleton skeleton__card-value dashboard-card__item-value" />
       </div>
-      <div className="skeleton__card dashboard-card__item rounded-xl bg-neutral-100 dark:bg-gray-900/10 backdrop-blur shadow-md border border-neutral-200 dark:border-none">
+      <div className="skeleton__card dashboard-card__item rounded-xl bg-neutral-100 dark:bg-gray-100/10 backdrop-blur shadow-md border border-neutral-200 dark:border-none">
         <span className="skeleton skeleton__card-label dashboard-card__item-label" />
         <span className="skeleton skeleton__card-value dashboard-card__item-value" />
       </div>
-      <div className="skeleton__card dashboard-card__item rounded-xl bg-neutral-100 dark:bg-gray-900/10 backdrop-blur shadow-md border border-neutral-200 dark:border-none">
+      <div className="skeleton__card dashboard-card__item rounded-xl bg-neutral-100 dark:bg-gray-100/10 backdrop-blur shadow-md border border-neutral-200 dark:border-none">
         <span className="skeleton skeleton__card-label dashboard-card__item-label" />
         <span className="skeleton skeleton__card-value dashboard-card__item-value" />
       </div>
-      <div className="skeleton__card dashboard-card__item rounded-xl bg-neutral-100 dark:bg-gray-900/10 backdrop-blur shadow-md border border-neutral-200 dark:border-none">
+      <div className="skeleton__card dashboard-card__item rounded-xl bg-neutral-100 dark:bg-gray-100/10 backdrop-blur shadow-md border border-neutral-200 dark:border-none">
         <span className="skeleton skeleton__card-label dashboard-card__item-label" />
         <span className="skeleton skeleton__card-value dashboard-card__item-value" />
       </div>
