a73134a719f32fee3b4d4fc4f6bd6b6f56f3a2be
bryanwaine <nwanne.ezeaka@gmail.com>
2025-07-20 00:17:34 +0100

    Replace copy symbol with copy entity

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="w-screen h-[8rem] flex justify-center items-center !p-[2rem] bg-[#001826]/10 backdrop-blur-md text-[#111111] dark:text-gray-400 border border-[0.5px] border-transparent border-t-[#666666] ">
-      © {new Date().getFullYear()}{" "}
+      &copy; {new Date().getFullYear()}{" "}
       <span className="!mx-[0.5rem] text-[1rem]">|</span> Built by{" "}
       <Link to="https://bryanwaine.netlify.app" target="_blank">
         <p className="text-[#FF7B00] !ml-[0.3rem] underline underline-offset-8">
